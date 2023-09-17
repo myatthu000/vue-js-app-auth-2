9e50f9e3c7295823bc9bd037edea718bbe29425a
myatthu000 <september.2019thu@gmail.com>
2023-09-17 15:38:46 +0630

    setup

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
